d124d5aee9a7df517130af39e1b93179cfc34ee6
ziedbarhoumi1989 <ziedbarhoumi1989@gmail.com>
2019-11-23 14:01:24 +0100

    graph refectored again

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -2,105 +2,107 @@
 
 // Instantiate a new graph
 var Graph = function(value) {
-	this.value= value;
-	this.edges=[]
+	this.value = value||'head';
+	this.edges = {}
 };
 
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
-	var tmp = new Graph(node);
-	tmp.edges.push(this);
-	this.edges.push(tmp);
+	var newNode = new Graph(node);
+	this.edges[node]=newNode;
+	newNode.edges[this.value]=this;
 };
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node) {
-	if(this.find(node)===undefined) {
-		return false
-	}
-	return true
-}
-Graph.prototype.find = function(node) {
-	var found = undefined;
-	var visited = [];
-	function search(obj){
-		visited.push(obj)
-		if(obj.value === node){
-
-			found = obj;
-			return true;
-		}
-		for(var i in obj.edges)  {
-			if(!visited.includes(obj.edges[i])) {
-				if(search(obj.edges[i]))
-					break;
-			}
-		}
-
-	}
-	search(this)
+ 	var found = false;
+	this.search(node,function(){
+		found = true;
+	});
 	return found;
 };
 
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
-	var remo=this.find(node);
-	remo.edges.forEach(function(edge){
-		edge.edges.splice(edge.edges.indexOf(remo),1)
+	var toBeRemoved;
+	this.search(node,function(obj){
+		toBeRemoved = obj
 	})
-	
-
+	for(var i in toBeRemoved.edges){
+		//console.log(toBeRemoved.edges[i])
+		delete toBeRemoved.edges[i].edges[toBeRemoved.value]
+	}
 };
+
+Graph.prototype.search = function(node, callback) {
+	var checked = {};
+
+	function findIt(obj){
+		if(!checked[obj.value]){
+			if(obj.value === node){
+				callback(obj)
+			}else{
+				checked[obj.value]=obj;
+				for(var i in obj.edges){
+					findIt(obj.edges[i]);
+					if(obj.edges[i].value===node)
+						break;
+				}
+			}
+		}
+	}
+	findIt(this);
+}
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
-	return this.find(fromNode).edges.includes(this.find(toNode))
+	var checkEdges;
+	this.search(fromNode, function(obj){
+		checkEdges = obj;
+	});
+	if(checkEdges.edges[toNode])
+		return true
+	return false
 };
 
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
-	var first = this.find(fromNode);
-	var second = this.find(toNode);
-	first.edges.push(second);
-	second.edges.push(first);
+	this.search(fromNode, function(obj){
+		fromNode = obj;
+	});
+	this.search(toNode, function(obj){
+		toNode = obj;
+	});
+	fromNode.edges[toNode.value] = toNode;
+	toNode.edges[fromNode.value] = fromNode;
+	
+	
 };
 
 // Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode) {
-	var first = this.find(fromNode);
-	var second = this.find(toNode);
-	first.edges.splice(first.edges.indexOf(second),1);
-	second.edges.splice(second.edges.indexOf(first),1);
+	this.search(fromNode, function(obj){
+		fromNode = obj;
+	});
+	var tmp = fromNode.edges[toNode];
+	delete fromNode.edges[toNode];
+	delete tmp.edges[fromNode.value];
 };
 
 // Pass in a callback which will be executed on each node of the graph.
 Graph.prototype.forEachNode = function(cb) {
-	var visited = [];
-	console.log(this)
-	search(this)
-	function search(obj){
-
-		console.log('happened', obj.edges)
-
-		visited.push(obj)
-		
-
-		for(var i in obj.edges)  {
-			if(!visited.includes(obj.edges[i])) {
-
-				search(obj.edges[i])
-				cb(obj.edges[i].value)
-				
+	var checked = {};
+	function findIt(obj){
+		if(!checked[obj.value]){		
+				cb(obj)
+				checked[obj.value]=obj;
+				for(var i in obj.edges){
+					findIt(obj.edges[i]);
 			}
 		}
-
 	}
-
-	
-	
+	findIt(this);
 };
-
-
 
 /*
  * Complexity: What is the time complexity of the above functions?
